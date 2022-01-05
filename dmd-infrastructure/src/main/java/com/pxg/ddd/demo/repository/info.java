c0066159b81da06dbb9bebd14e781867890d83e5
xgp <xgpan@foxmail.com>
2022-01-05 22:12:49 +0800

    #1 log4j

--- dmd-infrastructure/src/main/java/com/pxg/ddd/demo/repository/info.java
+++ dmd-infrastructure/src/main/java/com/pxg/ddd/demo/repository/info.java
@@ -1,4 +1,6 @@
 /**
- * @MapperScan is before @Mapper , so can avoid bean not found while inject the mapper to gatewayImpl
+ * @MapperScan works before @Mapper ,
+ * so can avoid Error bean not found while inject the mapper
+ * for files like gatewayImpl which use these mappers with @autowired and so on
  */
 
